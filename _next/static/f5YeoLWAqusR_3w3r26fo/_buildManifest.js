2a01f2ffe47f8c77177c4c74acadb60f142e49c6
ugnmura <54822569+ugnmura@users.noreply.github.com>
2025-02-12 16:37:14 +0000

    Deploying to gh-pages from @ ugnmura/Teacher-Simulator-Site@433e1f0371b273aef1d0dbac430b34cf305b1d76 🚀

---
+++ _next/static/f5YeoLWAqusR_3w3r26fo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9e390beaaa217ee9.js"],"/_error":["static/chunks/pages/_error-a090c2b3c01c0bad.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
